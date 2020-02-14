400b1f298d397f0d86fa453ae60d2db3b8f599e0
handes <2451322819@qq.com>
2020-02-14 18:21:16 +0800

    2020/2/14

--- different_dice.py
+++ different_dice.py
@@ -4,20 +4,20 @@ import pygal
 #创建一个D6
 #die = Die()
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 
 #掷几次骰子，并将结果存储在一个列表中
 results = []
-#for roll_num in range(100):
-for roll_num in range(1000):
-    #result = die.roll()
+
+for roll_num in range(50000):
+
     result = die_1.roll() + die_2.roll()
     results.append(result)
 
 #分析结果
 frequencies =[]
 max_result = die_1.num_sides + die_2.num_sides
-#for value in range(1, die.num_sides+1):
+
 for value in range(1, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
@@ -28,16 +28,16 @@ for value in range(1, max_result+1):
 #对结果进行可视化
 hist = pygal.Bar
 
-#hist.title = "Result of rolling one D6 1000 times."
-hist.title = "Result of rolling two D6 dice 1000 times."
-#hist.x_labels = ['1', '2', '3', '4', '5', '6']
-hist.x_labels = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+
+hist.title = "Result of rolling a D6 and a D10 50000 times."
+
+hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '12', '13', '14', '15', '16']
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
 
-#hist.add('D6', frequencies)
-hist.add('D6 + D6', frequencies)
+
+hist.add('D6 + D10', frequencies)
 hist.render_to_file('die_visual.svg')
 
 
